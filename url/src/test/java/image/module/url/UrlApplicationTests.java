521f707af295fee523bcfaa98e8fd3241f7ca9d9
MK <banggujoong@gmail.com>
2024-10-01 21:29:59 +0900

    remove .git files

---
+++ url/src/test/java/image/module/url/UrlApplicationTests.java
@@ -0,0 +1,13 @@
+package image.module.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
