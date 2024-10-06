cdd415e383683f69d0d295876af4ebcda6817b63
jiyeonahn <ajy7424@naver.com>
2024-10-06 17:15:58 +0900

    Feat: #10 @SneakyThrows 제거

--- upload/src/main/java/image/module/upload/UploadController.java
+++ upload/src/main/java/image/module/upload/UploadController.java
@@ -1,6 +1,15 @@
 package image.module.upload;
 
 import image.module.upload.domain.Image;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +32,8 @@ public class UploadController {
     //    치환된 이미지 이름은 UUID_업로드 요청시간
     //    ex) d5f59b88-2d5f-4d89-9e21-35fc2bcd0012_20231004123456
     @PostMapping
-    public ResponseEntity<UUID> uploadImage(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<UUID> uploadImage(@RequestParam("file") MultipartFile file)
+            throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         // 1. DB에 파일 메타데이터 저장 및 응답
         Image image = uploadService.saveImageMetadata(file);
         // 2. 비동기적으로 MinIO에 파일 저장
