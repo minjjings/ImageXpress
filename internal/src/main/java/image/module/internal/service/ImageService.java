3a3b94cb170cadcb3459e0c2296553cd11b363f8
mad-cost <mad_scientist1998@naver.com>
2024-10-09 23:14:38 +0900

    Feat: #29 이미지 리사이징

--- internal/src/main/java/image/module/internal/service/ImageService.java
+++ internal/src/main/java/image/module/internal/service/ImageService.java
@@ -6,6 +6,7 @@ import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,14 +41,15 @@ public class ImageService {
     InputStream originalFile = downloadImage(message);
     // 2. 원본 이미지 복사
     File copyOriginalFile = copyOriginalImage(originalFile);
-    // 3. 복사 이미지 WebP로 변환
-    File webpFile = convertToWebp(message, copyOriginalFile);
-    // 4. WebP 이미지 업로드
+    // 3. 복사 이미지를 300x300 리사이징
+    File resizedFile = resizeImage(copyOriginalFile, 300, 300);
+    // 4. 리사이즈된 이미지 WebP로 변환
+    File webpFile = convertToWebp(message, resizedFile);
+    // 5. WebP 이미지 업로드
     uploadWebPImage(message, webpFile);
-    // 5. 임시 파일 삭제
-    cleanupTemporaryFiles(copyOriginalFile, webpFile);
+    // 6. 임시 파일 삭제
+    cleanupTemporaryFiles(copyOriginalFile, resizedFile, webpFile);
   }
-
 
   // 이미지 다운로드
   public InputStream downloadImage(String fileName) {
@@ -67,7 +69,7 @@ public class ImageService {
   private File copyOriginalImage(InputStream originalFile) {
     File copyOriginalFile = null;
     try {
-      copyOriginalFile = File.createTempFile("original-", ".temp"); // 임시 파일 생성
+      copyOriginalFile = File.createTempFile("copy-", ".png"); // 임시 파일 생성
       FileUtils.copyInputStreamToFile(originalFile, copyOriginalFile); // 생성된 임시 파일에 원본 파일 복사
     } catch (IOException e) {
       throw new IllegalArgumentException("원본 이미지 복사 실패: " + e.getMessage());
@@ -75,15 +77,42 @@ public class ImageService {
     return copyOriginalFile;
   }
 
+  // 복사 이미지 리사이징
+  // 비율 고정 비율: 300x300
+  private File resizeImage(File originalFile, int width, int height) {
+    File resizedFile = new File(originalFile.getParent(), "stretched-" + originalFile.getName());
+    try {
+      // Thumbnails 라이브러리를 사용하여 지정한 크기로 왜곡
+      Thumbnails.of(originalFile)
+              .size(width, height) // 강제로 가로, 세로 크기 지정
+              .keepAspectRatio(false) // 이미지 비율 유지하지 않기
+              .toFile(resizedFile);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("이미지 리사이징 실패: " + e.getMessage());
+    }
+    return resizedFile;
+  }
+  // 이미지 비율 유지 ex) 1000x500 -> 300 x 150
+//  private File resizeImage(File originalFile, int width, int height) {
+//    File resizedFile = new File(originalFile.getParent(), "resized-" + originalFile.getName());
+//    try {
+//      // Thumbnails 라이브러리를 사용하여 이미지 리사이징
+//      Thumbnails.of(originalFile)
+//              .size(width, height)
+//              .toFile(resizedFile); // 리사이즈된 이미지를 저장
+//    } catch (IOException e) {
+//      throw new IllegalArgumentException("이미지 리사이징 실패: " + e.getMessage());
+//    }
+//    return resizedFile;
+//  }
+
   // 복사 이미지 WebP 파일로 변환
+  // 손실 압축
   public File convertToWebp(String fileName, File copyOriginalFile) {
     try {
-      return
-              // ImmutableImage 클래스를 사용하여 이미지 변환 작업을 위한 로더 객체 생성.
-              ImmutableImage.loader()
-                      .fromFile(copyOriginalFile)
-                      // TODO 파일 이름은 리사이즈 구현 후 다시 설정해주기
-                      .output(WebpWriter.DEFAULT, new File(copyOriginalFile.getParent(), fileName + ".webp"));
+      return ImmutableImage.loader()
+              .fromFile(copyOriginalFile)
+              .output(WebpWriter.DEFAULT, new File(copyOriginalFile.getParent(), FilenameUtils.getBaseName(fileName) + ".webp"));
     } catch (Exception e) {
       throw new RuntimeException(e.getMessage());
     }
@@ -114,5 +143,4 @@ public class ImageService {
       }
     }
   }
-
 }
