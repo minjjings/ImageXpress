c2289328912f6ae0b5e948c0469668c2ba30e6cd
MK <banggujoong@gmail.com>
2024-10-10 16:15:58 +0900

    Feat : #33 Data Feign Client 설정

--- data/src/main/java/image/module/data/presentation/ImageController.java
+++ data/src/main/java/image/module/data/presentation/ImageController.java
@@ -2,13 +2,10 @@ package image.module.data.presentation;
 
 import image.module.data.application.ImageResponse;
 import image.module.data.application.ImageService;
-import image.module.data.domain.Image;
-import image.module.data.domain.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +17,23 @@ public class ImageController {
     @PostMapping("/image/upload")
     public ImageResponse uploadImage(@RequestBody ImageRequest imageRequest){
         return imageService.createImage(imageRequest);
+    }
+
+    //fetch -> 객체 조회
+    @GetMapping("/image/getImageName")
+    public ImageResponse getImageName(@RequestParam UUID id){
+
+        ImageResponse getImageName = imageService.getImageName(id);
+
+        return getImageName;
+    }
+
+    //fetch -> cdn 주소로 객체 조회
+    @GetMapping("/image/getCDNImageName")
+    public ImageResponse getCDNImageName(@RequestParam String cdnUrl){
+
+        ImageResponse getCDNImageName = imageService.getCDNImageName(cdnUrl);
+
+        return getCDNImageName;
     }
 }
