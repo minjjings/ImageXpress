bc0f2bb551092fc6d3dc07e9d7292697ba11625a
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-10-10 12:49:12 +0900

    Feat: #32 CDN 캐싱 기능 구현
    redis에 값 넣을 때 ttl 설정하도록 구현
    redis에서 값 조회할 때 ttl 연장하도록 구현
    추후 FeignClient에서 캐싱 시간 받아서 설정하도록 수정 예정

--- cdn/src/main/java/image/module/cdn/service/RedisService.java
+++ cdn/src/main/java/image/module/cdn/service/RedisService.java
@@ -1,5 +1,6 @@
 package image.module.cdn.service;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,15 @@ public class RedisService {
     }
 
     // 값을 Redis에 저장하는 메서드
-    public void setValue(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+    public void setValue(String key, String value, int cachingTime) {
+        redisTemplate.opsForValue().set(key + ":ttl", String.valueOf(cachingTime));
+        redisTemplate.opsForValue().set(key, value, cachingTime, TimeUnit.MINUTES);
     }
 
     // 값을 Redis에서 가져오는 메서드
     public String getValue(String key) {
+        String initialTTL = redisTemplate.opsForValue().get(key + ":ttl");
+        redisTemplate.expire(key, Integer.parseInt(initialTTL), TimeUnit.MINUTES);
         return redisTemplate.opsForValue().get(key);
     }
 }
