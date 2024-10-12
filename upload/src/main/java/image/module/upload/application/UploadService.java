37bb129a57c9042972d7a045c6298e30f01d786a
jiyeonahn <ajy7424@naver.com>
2024-10-12 20:11:45 +0900

    Refactor: #40 이미지 업로드 응답 속도 개선

--- upload/src/main/java/image/module/upload/application/UploadService.java
+++ upload/src/main/java/image/module/upload/application/UploadService.java
@@ -3,20 +3,22 @@ package image.module.upload.application;
 import image.module.upload.util.FileUtil;
 import image.module.upload.domain.ImageExtension;
 import image.module.upload.infrastructure.DataService;
+import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -33,61 +35,89 @@ public class UploadService {
     @Value("${cdn-server.url}")
     private String cdnBaseUrl;
 
-    //이미지 데이터 db 저장
-    @Transactional
-    public ImageResponse saveImageMetadata(MultipartFile file) throws Exception {
-        // 업로드 파일명을 불러옴
-        String originalName = file.getOriginalFilename();
+    public CompletableFuture<ImageResponse> saveImageMetadata(MultipartFile file) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                // 업로드 파일명을 불러옴
+                String originalName = file.getOriginalFilename();
 
-        // 파일 이름이 존재하지 않는 경우
-        if(originalName == null) throw new IllegalArgumentException("잘못된 파일입니다.");
+                // 파일 이름이 존재하지 않는 경우
+                if (originalName == null) throw new IllegalArgumentException("잘못된 파일입니다.");
 
-        // test_image.jpg > [0] -> test_image, [1] -> jpg
-        String[] fileInfos = FileUtil.splitFileName(file.getOriginalFilename());
+                // 파일 정보 분리
+                String[] fileInfos = FileUtil.splitFileName(originalName);
 
-        // ImageExtension Enum 에 설정한 확장자가 아닌 경우는 업로드 하지 않음
-        ImageExtension extension = ImageExtension.findByKey(fileInfos[1])
-                .orElseThrow(() -> new Exception("지원하지 않는 확장자 입니다."));
+                // 확장자 체크
+                ImageExtension extension = ImageExtension.findByKey(fileInfos[1])
+                        .orElseThrow(() -> new Exception("지원하지 않는 확장자 입니다."));
 
-        int imageWidth = 0;
-        int imageHeight = 0;
-        try {
-            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
-            imageWidth = bufferedImage.getWidth();
-            imageHeight = bufferedImage.getHeight();
-        } catch (IOException e) {
-            log.error("IOException 발생: 이미지 입력 스트림을 읽을 수 없습니다.", e);
-        }
+                // 이미지 크기 확인
+                BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+                int imageWidth = bufferedImage.getWidth();
+                int imageHeight = bufferedImage.getHeight();
 
-        ImageRequest imageRequest = ImageRequest.create(
-                originalName,
-                extension.getKey(),
-                cdnBaseUrl,
-                imageWidth,
-                imageHeight
-        );
-        return dataService.uploadImage(imageRequest);
+                ImageRequest imageRequest = ImageRequest.create(
+                        originalName,
+                        extension.getKey(),
+                        cdnBaseUrl,
+                        imageWidth,
+                        imageHeight
+                );
+                return dataService.uploadImage(imageRequest);
+            } catch (Exception e) {
+                log.error("이미지 메타데이터 저장 중 오류 발생: ", e);
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    //이미지 업로드
-    @Async
+    // 이미지 업로드
     public void uploadImage(MultipartFile file, ImageResponse image) {
-        try {
-            minioClient.putObject(
-                    PutObjectArgs.builder()
-                            .bucket(bucketName)
-                            .object(image.getStoredFileName())
-                            .stream(file.getInputStream(), file.getSize(), -1)
-                            .contentType(file.getContentType())
-                            .build()
-            );
-        } catch (IOException e){
-            log.error("IOException 발생: 이미지 입력 스트림을 읽을 수 없습니다.", e);
-        }
-        catch (Exception e) {
-            log.error("Exception 발생: ", e);
-        }
+        CompletableFuture.runAsync(() -> {
+            try {
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(image.getStoredFileName())
+                                .stream(file.getInputStream(), file.getSize(), -1)
+                                .contentType(file.getContentType())
+                                .build()
+                );
+                kafkaTemplate.send("image-upload-topic", image.getStoredFileName());
+            } catch (Exception e) {
+                log.error("이미지 업로드 중 오류 발생: ", e);
+            }
+        });
+    }
 
-        kafkaTemplate.send("image-upload-topic", image.getStoredFileName());
+    @SneakyThrows
+    public ResponseEntity<byte[]> getImage(String cdnUrl) {
+        byte[] imageBytes = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object("")
+                        .build()).readAllBytes();
+
+        // 응답 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("image/jpeg"));
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+    }
+
+    @SneakyThrows
+    public ResponseEntity<byte[]> downloadImage(String cdnUrl){
+        byte[] imageBytes = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object("")
+                        .build()).readAllBytes();
+
+        // 응답 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("image/jpeg"));
+        headers.setContentDispositionFormData("attachment", "test.jpeg");
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
     }
 }
--- upload/src/main/java/image/module/upload/presentation/UploadController.java
+++ upload/src/main/java/image/module/upload/presentation/UploadController.java
@@ -2,9 +2,15 @@ package image.module.upload.presentation;
 
 import image.module.upload.application.UploadService;
 import image.module.upload.application.ImageResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,14 +28,28 @@ public class UploadController {
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
             // 1. DB에 파일 메타데이터 저장 및 응답
-            ImageResponse imageResponse = uploadService.saveImageMetadata(file);
-            // 2. 비동기적으로 MinIO에 파일 저장
-            uploadService.uploadImage(file, imageResponse);
+            CompletableFuture<ImageResponse> imageResponseFuture = uploadService.saveImageMetadata(file);
 
-            return ResponseEntity.ok(imageResponse.getOriginalFileUUID());
+            // 2. 비동기적으로 MinIO에 파일 저장
+            imageResponseFuture.thenAccept(imageResponse ->
+                    uploadService.uploadImage(file, imageResponse)
+            );
+
+            // 즉시 응답 반환
+            return ResponseEntity.ok("이미지 업로드가 시작되었습니다.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Upload failed: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<byte[]> getImage(HttpServletRequest request) {
+        return uploadService.getImage(request.getRequestURL().toString());
+    }
+
+    @GetMapping("/downloadTest")
+    public ResponseEntity<byte[]> downloadImage(HttpServletRequest request) {
+        return uploadService.downloadImage(request.getRequestURL().toString());
     }
 }
