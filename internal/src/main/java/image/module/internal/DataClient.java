4ded7f5663c01fbe1848e580cf9dddaa64a9b70e
mad-cost <mad_scientist1998@naver.com>
2024-10-15 15:30:55 +0900

    Refactor : #48- PocessingServer Eureka연결 및 yml 분리

--- internal/src/main/java/image/module/internal/DataClient.java
+++ internal/src/main/java/image/module/internal/DataClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "data-service", url = "${data-server.url}")
+@FeignClient(name = "data-service")
 public interface DataClient {
 
   @PostMapping("/image/upload/resize")
