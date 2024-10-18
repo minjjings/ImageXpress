13e9a13930472851ff8564288ede6e618110859f
mad-cost <mad_scientist1998@naver.com>
2024-10-18 12:26:50 +0900

    Chore: #57 yml 설정

--- convert/src/main/java/image/module/convert/ConvertApplication.java
+++ convert/src/main/java/image/module/convert/ConvertApplication.java
@@ -2,7 +2,10 @@ package image.module.convert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ConvertApplication {
 
