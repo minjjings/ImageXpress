734802bc9bfc9d929be53c0b91cd20ea2b998028
mad-cost <mad_scientist1998@naver.com>
2024-10-21 01:36:08 +0900

    Init: #70 resizeServer Init

---
+++ resize/src/test/java/image/module/resize/ResizeApplicationTests.java
@@ -0,0 +1,13 @@
+package image.module.resize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
