880d7ca9f1f52758dbd0284174197961b1624deb
mad-cost <mad_scientist1998@naver.com>
2024-10-21 01:55:00 +0900

    Chore: #70 build.gradle, application.yml, docker-compose.yml 설정 및 Eureka 연결하기

--- resize/src/main/java/image/module/resize/ResizeApplication.java
+++ resize/src/main/java/image/module/resize/ResizeApplication.java
@@ -2,7 +2,9 @@ package image.module.resize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ResizeApplication {
 
