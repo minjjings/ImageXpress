0c803b8575887c88f5b67a21f2180ce3c6c09293
MK <banggujoong@gmail.com>
2024-11-08 13:44:12 +0900

    [FEAT] resizing delete

--- resize/src/main/java/image/module/upload/infrastructure/ConvertService.java
+++
@@ -1,8 +0,0 @@
-package image.module.upload.infrastructure;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-
-public interface ConvertService {
-    ResponseEntity<byte[]> getRedisImageByte(@RequestParam String imageName);
-}
--- resize/src/main/java/image/module/upload/infrastructure/DataService.java
+++
@@ -1,8 +0,0 @@
-package image.module.upload.infrastructure;
-
-import image.module.upload.dto.ImageRequest;
-import image.module.upload.dto.ImageResponse;
-
-public interface DataService {
-    ImageResponse uploadImage(ImageRequest imageRequest);
-}
