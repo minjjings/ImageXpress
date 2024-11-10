8900b540068b10203033cded611e895928dc270c
MK <banggujoong@gmail.com>
2024-11-11 03:49:00 +0900

    [FEAT] #7 cdn 조회

--- cdn/src/main/java/image/module/cdn/config/RedisExpirationListener.java
+++ cdn/src/main/java/image/module/cdn/config/RedisExpirationListener.java
@@ -1,58 +1,58 @@
-package image.module.cdn.config;
-
-import image.module.cdn.service.RedisService;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class RedisExpirationListener implements MessageListener {
-
-    @Value("${cdn.image.path}")
-    public String filePath;
-
-    private final RedisService redisService;
-
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
-        String expiredKey = message.toString();
-        log.info("Expired Key: " + expiredKey);
-
-        // 여기서 TTL 만료 시 실행할 메서드를 호출
-        try {
-            handleExpiredKey(expiredKey);
-        } catch (IOException e) {
-            log.error("이미지 삭제 시 오류 발생!! ");
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void handleExpiredKey(String key) throws IOException {
-        // TTL 만료 시 실행할 비즈니스 로직
-        String value = redisService.getValue("backup_" + key);
-
-        // value에서 경로 받아 이미지 삭제
-        deleteImage(value);
-
-        redisService.deleteKey("backup_" + key);
-        redisService.deleteKey(key + ":ttl");
-        redisService.deleteKey(key + ":hitRate");
-    }
-
-    private void deleteImage(String imageName) throws IOException {
-        log.info(imageName + " 이미지 삭제 시도");
-        Path imagePath = Paths.get(imageName);
-        Files.deleteIfExists(imagePath);
-        log.info("이미지 삭제 성공");
-    }
-}
+//package image.module.cdn.config;
+//
+//import image.module.cdn.service.RedisService;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.data.redis.connection.Message;
+//import org.springframework.data.redis.connection.MessageListener;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class RedisExpirationListener implements MessageListener {
+//
+//    @Value("${cdn.image.path}")
+//    public String filePath;
+//
+//    private final RedisService redisService;
+//
+//    @Override
+//    public void onMessage(Message message, byte[] pattern) {
+//        String expiredKey = message.toString();
+//        log.info("Expired Key: " + expiredKey);
+//
+//        // 여기서 TTL 만료 시 실행할 메서드를 호출
+//        try {
+//            handleExpiredKey(expiredKey);
+//        } catch (IOException e) {
+//            log.error("이미지 삭제 시 오류 발생!! ");
+//            log.error(e.getMessage());
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    private void handleExpiredKey(String key) throws IOException {
+//        // TTL 만료 시 실행할 비즈니스 로직
+//        String value = redisService.getValue("backup_" + key);
+//
+//        // value에서 경로 받아 이미지 삭제
+//        deleteImage(value);
+//
+//        redisService.deleteKey("backup_" + key);
+//        redisService.deleteKey(key + ":ttl");
+//        redisService.deleteKey(key + ":hitRate");
+//    }
+//
+//    private void deleteImage(String imageName) throws IOException {
+//        log.info(imageName + " 이미지 삭제 시도");
+//        Path imagePath = Paths.get(imageName);
+//        Files.deleteIfExists(imagePath);
+//        log.info("이미지 삭제 성공");
+//    }
+//}
--- cdn/src/main/java/image/module/cdn/controller/CdnController.java
+++ cdn/src/main/java/image/module/cdn/controller/CdnController.java
@@ -1,17 +1,13 @@
 package image.module.cdn.controller;
 
-import image.module.cdn.dto.ImageResponseDto;
 import image.module.cdn.service.CdnService;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,14 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class CdnController {
 
     private final CdnService cdnService;
+    @GetMapping("/{imageType}/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName, @PathVariable String imageType) {
 
-    @GetMapping("/{cdnImageName}")
-    public ResponseEntity<String> getImage(@RequestParam String imagName,
-                                           @RequestParam String imageSize) {
 
-//        cdnService.getImage(imagName,imageSize);
 
-     return ResponseEntity.ok("이미지 조회 완료 ");
+
+       byte[] imageBytes= cdnService.getImage(imageName,imageType).getBody();
+
+        if (imageBytes == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(("Image not found: " + imageName).getBytes());
+        }
+
+        // 이미지 응답 반환 (무조건 image/jpeg로 설정)
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)  // image/jpeg로 고정
+                .body(imageBytes);
     }
 
 
